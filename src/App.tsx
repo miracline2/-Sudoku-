bea115dfe736207d194bddebf795e66cc5761593
miracline2 <francesca4518@gmail.com>
2025-04-25 20:01:29 +0530

    Hello Card in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
 // src/App.tsx
-import React, { useState } from "react";
-import { initialPuzzle, SudokuGrid } from "./data/puzzle";
-import SudokuBoard from "./components/SudokuBoard";
-import { isValidSudoku } from "./utils/validate";
+import React from "react";
+import HelloCard from "./components/HelloCard";
+
 
 const App: React.FC = () => {
-  const [grid, setGrid] = useState<SudokuGrid>(JSON.parse(JSON.stringify(initialPuzzle)));
-  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
-  const handleCellChange = (row: number, col: number, value: number | null) => {
-    const newGrid = [...grid];
-    newGrid[row][col] = { ...newGrid[row][col], value };
-    setGrid(newGrid);
-  };
-
-  const checkSolution = () => {
-    setIsCorrect(isValidSudoku(grid));
-  };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4 text-center">
-      <h1 className="text-2xl font-bold mb-4">🧠 Mini Sudoku (4x4)</h1>
-      <SudokuBoard grid={grid} onCellChange={handleCellChange} />
-      <button
-        onClick={checkSolution}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded cursor-pointer"
-      >
-        Check Solution
-      </button>
-      {isCorrect !== null && (
-        <p className={`mt-4 text-lg font-semibold ${isCorrect ? "text-green-600" : "text-red-600"}`}>
-          {isCorrect ? "✅ You solved it!" : "❌ Incorrect. Try again!"}
-        </p>
-      )}
+    <div className="min-h-screen bg-gradient-to-r from-[135deg] via-[#ffd6e8] to-[#ffe4f0] p-4 text-center flex justify-center items-center">
+     <HelloCard/>
+    
     </div>
   );
 };
---
+++ src/components/HelloCard.tsx
@@ -0,0 +1,13 @@
+
+
+const HelloCard = () => {
+  return (
+    <div className=" w-100 h-85 bg-white border-0 shadow-md flex justify-center items-center ">
+        <h2 className="text-center  font-bold mx-auto">Hello World!! This is Miracline 🧁</h2>
+      
+    </div>
+  )
+}
+
+export default HelloCard
+
